6b12f0ad06ce0ed1ccd0114949989a7439823c13
Puneet Chaudhary <puneet.chaudhary@version1.com>
2019-02-06 11:40:00 +0000

    index.js and html pages added

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res) =>{
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+app.listen(3018);
+console.log("Listening on port 3018");
